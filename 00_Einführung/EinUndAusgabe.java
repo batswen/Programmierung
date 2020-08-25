b68c793619a98259fbf9c772e3698ec189d1e22d
batswen <batswen@gmail.com>
2020-08-25 23:19:26 +0200

    update

--- 00_Einführung/EinUndAusgabe.java
+++ 00_Einführung/EinUndAusgabe.java
@@ -9,10 +9,6 @@ public class EinUndAusgabe {
         Scanner scanner = new Scanner(System.in);
         String eingabe;
 
-        System.out.print("Text oder Variable!!!   ");
-        System.out.printf("Zeichen: %c, Zeichenkette: %s, Int: %d\n", 'a', "Xyz", 12345);
-        System.out.println("Ausgabe mit Zeilenvorschub");
-
         eingabe = scanner.nextLine();
         System.out.println("Sie haben " + eingabe + " geschrieben!");
         System.out.printf("Sie haben %s geschrieben!\n", eingabe);
